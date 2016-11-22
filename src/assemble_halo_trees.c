41866601a537ce5f3d159d0364ef7d7082da43a5
Peter Behroozi <peter@Peters-MacBook-Pro.local>
2016-11-21 16:51:28 -0800

    Change Snap_num label to Snap_idx

--- src/assemble_halo_trees.c
+++ src/assemble_halo_trees.c
@@ -324,7 +324,7 @@ int64_t create_headers(void) {
 	id = i*BOX_DIVISIONS*BOX_DIVISIONS + j*BOX_DIVISIONS + k;
 	output = tree_outputs[id] = check_fopen(buffer, "w");
 	fprintf(output,
-		"#scale(0) id(1) desc_scale(2) desc_id(3) num_prog(4) pid(5) upid(6) desc_pid(7) phantom(8) sam_M%s(9) M%s(10) R%s(11) rs(12) vrms(13) mmp?(14) scale_of_last_MM(15) vmax(16) x(17) y(18) z(19) vx(20) vy(21) vz(22) Jx(23) Jy(24) Jz(25) Spin(26) Breadth_first_ID(27) Depth_first_ID(28) Tree_root_ID(29) Orig_halo_ID(30) Snap_num(31) Next_coprogenitor_depthfirst_ID(32) Last_progenitor_depthfirst_ID(33) Last_mainleaf_depthfirst_ID(34) Tidal_Force(35) Tidal_ID(36) %s\n"
+		"#scale(0) id(1) desc_scale(2) desc_id(3) num_prog(4) pid(5) upid(6) desc_pid(7) phantom(8) sam_M%s(9) M%s(10) R%s(11) rs(12) vrms(13) mmp?(14) scale_of_last_MM(15) vmax(16) x(17) y(18) z(19) vx(20) vy(21) vz(22) Jx(23) Jy(24) Jz(25) Spin(26) Breadth_first_ID(27) Depth_first_ID(28) Tree_root_ID(29) Orig_halo_ID(30) Snap_idx(31) Next_coprogenitor_depthfirst_ID(32) Last_progenitor_depthfirst_ID(33) Last_mainleaf_depthfirst_ID(34) Tidal_Force(35) Tidal_ID(36) %s\n"
 		"#Omega_M = %f; Omega_L = %f; h0 = %f\n"
 		"#Full box size = %f Mpc/h\n"
 		"#Scale: Scale factor of halo.\n"
@@ -352,7 +352,7 @@ int64_t create_headers(void) {
 		"#Depth_first_ID: depth-first ordering of halos within a tree.\n"
 		"#Tree_root_ID: ID of the halo at the last timestep in the tree.\n"
 		"#Orig_halo_ID: Original halo ID from halo finder.\n"
-		"#Snap_num: Snapshot number from which halo originated.\n"
+		"#Snap_idx: Index of snapshot (in original snapshot list) from which halo originated.\n"
 		"#Next_coprogenitor_depthfirst_ID: Depthfirst ID of next coprogenitor.\n"
 		"#Last_progenitor_depthfirst_ID: Depthfirst ID of last progenitor.\n"
 		"#Last_mainleaf_depthfirst_ID: Depthfirst ID of last progenitor on main progenitor branch.\n"
